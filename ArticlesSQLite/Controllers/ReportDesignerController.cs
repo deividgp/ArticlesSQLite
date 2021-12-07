327ed1b957809182669e4fd77c51ca80394d9a8f
David Gutiérrez <dayd4@outlook.com>
2021-12-07 16:52:44 +0100

    Reporting

---
+++ ArticlesSQLite/Controllers/ReportDesignerController.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.Net6.BlazorIntegrationDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using Telerik.Reporting.Services;
+    using Telerik.WebReportDesigner.Services;
+    using Telerik.WebReportDesigner.Services.Controllers;
+
+    [Route("api/reportdesigner")]
+    [ApiController]
+    public class ReportDesignerController : ReportDesignerControllerBase
+    {
+        public ReportDesignerController(IReportDesignerServiceConfiguration reportDesignerServiceConfiguration, IReportServiceConfiguration reportServiceConfiguration)
+            : base(reportDesignerServiceConfiguration, reportServiceConfiguration)
+        {
+        }
+    }
+}
--- ArticlesSQLite/Program.cs
+++ ArticlesSQLite/Program.cs
@@ -2,9 +2,14 @@ using ArticlesSQLite;
 using ArticlesSQLite.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Telerik.Reporting.Cache.File;
+using Telerik.Reporting.Services;
+using Telerik.WebReportDesigner.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -14,6 +19,21 @@ builder.Services.AddTelerikBlazor();
 
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContextFactory<ArticlesDbContext>();
+
+builder.Services.TryAddSingleton<IReportServiceConfiguration>(sp => new ReportServiceConfiguration
+{
+    ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
+    HostAppId = "ArticlesSQLite",
+    Storage = new FileStorage(),
+    ReportSourceResolver = new UriReportSourceResolver(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Reports"))
+});
+
+builder.Services.TryAddSingleton<IReportDesignerServiceConfiguration>(sp => new ReportDesignerServiceConfiguration
+{
+    DefinitionStorage = new FileDefinitionStorage(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Reports")),
+    SettingsStorage = new FileSettingsStorage(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Telerik Reporting")),
+    ResourceStorage = new ResourceStorage(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Resources"))
+});
 
 var app = builder.Build();
 
@@ -30,6 +50,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.MapControllers();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
